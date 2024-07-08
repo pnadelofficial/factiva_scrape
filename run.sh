3f1948fc7193c650b408adfdd19897c56225e79b
peter nadel <peter.m.nadel@gmail.com>
2024-07-08 09:46:09 -0400

    default arg for run.sh plus making it runnable on clone

--- run.sh
+++ run.sh
@@ -2,7 +2,7 @@ conda create -n scrape python=3.11
 conda activate scrape
 pip install -r requirements.txt 
 
-PAGE_ARG = $1
+PAGE_ARG = ${1:-10}
 
 echo "Running the script"
 python factiva_scrape.py "$PAGE_ARG"
